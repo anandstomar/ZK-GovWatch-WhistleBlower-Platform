9cf19957fbb3392b6d63ba37a0627566bf54a091
anandstomar <anandsinghtomar25.com>
2026-02-03 22:13:58 +0530

    second commits

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,21 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* We remove the invalid 'experimental: { turbo }' block */
 
-const nextConfig: NextConfig = {
-  // webpack: (config) => {
-  //   config.resolve.fallback = {
-  //     fs: false,       // Semaphore uses node-fs which breaks in browser
-  //     readline: false, // Fixes other node-specific dependencies
-  //   };
-  //   return config;
-  // },
+  /* Keep this! It is required for snarkjs to work in the browser */
+  webpack: (config, { isServer  }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        readline: false,
+        os: false,
+        path: false,
+        constants: false,
+        stream: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
